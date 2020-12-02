596281f2a44de1307f6b8c5cc19e7820070605fe
Roberta <robertasciortino.ag@gmail.com>
2020-12-02 16:00:43 +0100

    Vue main footer recent posts e tags

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+var app = new Vue ({
+  el: '#root',
+  data: {
+    recentPosts: [
+      'Aenean lobortis sapien enim viverra',
+      'Duis ac massa semper maximus',
+      'Nunc fermint nulla eu justo sem id',
+      'Aliquam posuere magna eget nibh',
+      'Cras ac nulla ac consecte rutrum',
+      'Fusce mattis nunc ut aliquam'
+    ],
+    tags: [
+      'Black', 'boots', 'Brown', 'Casual', 'D&G', 'Fabric', 'flowers','Grey', 'hat', 'Hipster', 'lines', 'multi-purpose', 'New York', 'Outdoors', 'red', 'responsive', 'summer', 'sweater', 'Travel', 'Warm', 'White', 'winter'
+    ]
+  }
+});
