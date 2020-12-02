56d865ea8a899cf558d869c64c406aabacfd02f1
Roberta <robertasciortino.ag@gmail.com>
2020-12-02 17:44:08 +0100

    Vue footer bottom

--- assets/main.js
+++ assets/main.js
@@ -86,6 +86,12 @@ var app = new Vue ({
     ],
     tags: [
       'Black', 'boots', 'Brown', 'Casual', 'D&G', 'Fabric', 'flowers','Grey', 'hat', 'Hipster', 'lines', 'multi-purpose', 'New York', 'Outdoors', 'red', 'responsive', 'summer', 'sweater', 'Travel', 'Warm', 'White', 'winter'
+    ],
+    copyright: [
+      '©Copyright 2012-2020',
+      'Avada Theme by Theme Fusion',
+      'All Rights Reserved',
+      'Powered By Wordpress'
     ]
   }
 });
